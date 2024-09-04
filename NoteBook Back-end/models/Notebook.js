6fa3fb4f6ff5e390b5fd546ceebb047e693eed1c
watch14 <watchman1414@gmail.com>
2024-09-04 09:20:09 +0100

    notebook schema

---
+++ NoteBook Back-end/models/Notebook.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const NotebookSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    theme: {
+      type: String,
+      required: false,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Notebook", NotebookSchema);
