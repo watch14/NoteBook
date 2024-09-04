0a5feedec66ab4d6fb27ef3e8b60ff3b822adc74
watch14 <watchman1414@gmail.com>
2024-09-04 10:24:10 +0100

    NoteBook CRUD implemented

--- NoteBook Back-end/controllers/notebook.controller.js
+++ NoteBook Back-end/controllers/notebook.controller.js
@@ -24,3 +24,75 @@ export const createNotebook = async (req, res, next) => {
     );
   }
 };
+
+// Get notebook by ID
+export const getNotebookById = async (req, res, next) => {
+  try {
+    const notebookId = req.params.id;
+    const notebook = await Notebook.findById(notebookId);
+    if (!notebook) {
+      return next(CreateError(404, "Notebook Not Found!"));
+    }
+
+    return next(CreateSuccess(200, "Notebook fetched successfully", notebook));
+  } catch (err) {
+    return next(
+      CreateError(500, "Internal Server Error for fetching a Notebook!")
+    );
+  }
+};
+
+// Get all notebooks
+export const getAllNotebooks = async (req, res, next) => {
+  try {
+    const notebooks = await Notebook.find();
+    return next(
+      CreateSuccess(200, "All notebooks fetched successfully", notebooks)
+    );
+  } catch (err) {
+    return next(
+      CreateError(500, "Internal Server Error for fetching all Notebooks!")
+    );
+  }
+};
+
+// Update notebook
+export const updateNotebook = async (req, res, next) => {
+  try {
+    const notebookId = req.params.id;
+    const notebook = await Notebook.findById(notebookId);
+
+    if (!notebook) {
+      return next(CreateError(404, "Notebook Not Found!"));
+    }
+
+    const updatedNotebook = await Notebook.findByIdAndUpdate(
+      notebookId,
+      { $set: req.body },
+      { new: true }
+    );
+
+    return next(CreateSuccess(200, "Notebook Updated!", updatedNotebook));
+  } catch (err) {
+    return next(
+      CreateError(500, "Internal Server Error for Updating a Notebook!")
+    );
+  }
+};
+
+// Delete notebook
+export const deleteNotebook = async (req, res, next) => {
+  try {
+    const notebookId = req.params.id;
+    const notebook = await Notebook.findById(notebookId);
+    if (!notebook) {
+      return next(CreateError(404, "Notebook Not Found!"));
+    }
+    await Notebook.findByIdAndDelete(notebookId);
+    return next(CreateSuccess(200, "Notebook Deleted!", notebook));
+  } catch (err) {
+    return next(
+      CreateError(500, "Internal Server Error for Deleting a Notebook!")
+    );
+  }
+};
--- NoteBook Back-end/routes/notebook.js
+++ NoteBook Back-end/routes/notebook.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { createNotebook } from "../controllers/notebook.controller.js";
+import {
+  createNotebook,
+  deleteNotebook,
+  getAllNotebooks,
+  getNotebookById,
+  updateNotebook,
+} from "../controllers/notebook.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createNotebook);
+
+router.get("/get/:id", getNotebookById);
+
+router.get("/all", getAllNotebooks);
+
+router.put("/update/:id", updateNotebook);
+
+router.delete("/delete/:id", deleteNotebook);
 
 export default router;
