82b876fc92547bccc1297675aa17e8cef92395c6
watch14 <watchman1414@gmail.com>
2024-09-12 17:10:32 +0100

    Add translation functionality to the translate controller

--- NoteBook Back-end/controllers/trasnlate.controller.js
+++ NoteBook Back-end/controllers/trasnlate.controller.js
@@ -1,2 +1,19 @@
 import { CreateSuccess } from "../utils/success.js";
 import { CreateError } from "../utils/error.js";
+import convertAndTranslate from "../utils/translateEng.js";
+
+// Translate
+export const translate = async (req, res, next) => {
+  try {
+    const { text } = req.body;
+    if (!text) {
+      return next(CreateError(400, "text is required!"));
+    }
+
+    const result = await convertAndTranslate(text);
+
+    return next(CreateSuccess(200, "Translation successful!", result));
+  } catch (err) {
+    return next(CreateError(500, "Internal server error for translating!"));
+  }
+};
