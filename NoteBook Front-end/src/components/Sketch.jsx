61e37530f34dea6457c74c1568773f71b59aecb6
watch14 <watchman1414@gmail.com>
2024-09-16 15:58:09 +0100

    Add Excalidraw dependency and component; implement Sketch component for drawing functionality

---
+++ NoteBook Front-end/src/components/Sketch.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Excalidraw } from "@excalidraw/excalidraw";
+import "@excalidraw/excalidraw/dist/excalidraw.min.css";
+
+const Sketch = () => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <Excalidraw />
+    </div>
+  );
+};
+
+export default Sketch;
