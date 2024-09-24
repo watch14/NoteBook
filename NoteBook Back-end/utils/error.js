95458c290500f9e536910d51351ab9a4b994148f
watch14 <watchman1414@gmail.com>
2024-09-24 02:46:39 +0100

    Improve error logging format in CreateError function; use delimiters for better readability

--- NoteBook Back-end/utils/error.js
+++ NoteBook Back-end/utils/error.js
@@ -2,6 +2,8 @@ export const CreateError = (status, message) => {
   const err = new Error();
   err.status = status;
   err.message = message;
-  console.error(`ERROR:${new Date().toISOString()} | ${status} | ${message}`);
+  console.error(
+    `ERROR:${new Date().toISOString()} |||| ${status} |||| ${message}`
+  );
   return err;
 };
