b9756ad7ff8e162e8d1e62b7d6c7a5141d4df947
watch14 <watchman1414@gmail.com>
2024-10-01 14:26:20 +0100

    Fix Header component: correct Sun icon rendering for light mode

--- NoteBook Front-end/src/components/Header.jsx
+++ NoteBook Front-end/src/components/Header.jsx
@@ -106,7 +106,7 @@ function Header() {
             {theme === "light" ? (
               <Moon size={20} /> // Show moon icon for dark mode
             ) : (
-              <Sun Moon size={20} /> // Show sun icon for light mode
+              <Sun size={20} /> // Show sun icon for light mode
             )}
           </button>
         </nav>
