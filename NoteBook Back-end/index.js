06b214fdc3536618ac2787e76b53a74aa92ebd97
watch14 <watchman1414@gmail.com>
2024-10-02 10:38:44 +0100

    Update CORS configuration to allow multiple origins for frontend access

--- NoteBook Back-end/index.js
+++ NoteBook Back-end/index.js
@@ -21,8 +21,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Adjust this to your frontend's URL
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allow these methods
+    origin: ["https://notebook-back-end.onrender.com", "http://localhost:5173"], // Allow your backend and frontend URLs    methods: ["GET", "POST", "PUT", "DELETE"], // Allow these methods
     allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
   })
 );
